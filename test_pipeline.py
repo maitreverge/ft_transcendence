b8f0db4f9cf23637eb9e74d4a5a22e7ebfe5a3c4
Dan Sylvain <dansylvain84@gmail.com>
2025-01-27 06:53:43 +0100

    new test

--- test_pipeline.py
+++ test_pipeline.py
@@ -1,9 +1,9 @@
-def mauvaise_fonction(  ):
-    print("Ceci est mal formaté"    )
-    x     =        42
+    # def mauvaise_fonction(  ):
+    #     print("Ceci est mal formaté"    )
+    #     x     =        42
 
-def main()
-    print("Mon nez dans ton fu")
+    # def main()
+    #     print("Mon nez dans ton fu")
 
-if __name__ == "__main__"
-    main()
+    # if __name__ == "__main__"
+    #     main()
