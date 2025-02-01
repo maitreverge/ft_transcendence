c014c53495e2a249a464fbe147f77b46ac8de405
sierra182 <sierra-echo@msn.com>
2025-02-01 19:40:59 +0100

    collectstatic only on prod

--- ultimate_project/static_files/entrypoint.sh
+++ ultimate_project/static_files/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-mkdir -p /app/staticfiles && chmod -R 777 /app/staticfiles
-python manage.py collectstatic --noinput
-
 if [ "${env}" = "prod" ]; then \
+	mkdir -p /app/staticfiles && chmod -R 777 /app/staticfiles; \
+	python manage.py collectstatic --noinput; \
 	uvicorn static_files.asgi:application --host 0.0.0.0 --port 8000; \
 else \
 	python ./manage.py runserver 0.0.0.0:8000; \
