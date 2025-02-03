a2f4ccf22eea2000cb0bbd592cc71e9ff283e923
Dan SYLVAIN <dansylvain84@gmail.com>
2025-02-03 13:54:42 +0100

    linting ok

--- ultimate_project/static_files/static_files/settings.py
+++ ultimate_project/static_files/static_files/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = \
-    "django-insecure-yhu)0t6)-j5#_5wtcyi6v^a4h9)skb*%t44wt+v%=2jz&x#i$q"
+SECRET_KEY = "django-insecure-yhu)0t6)\
+    -j5#_5wtcyi6v^a4h9)skb*%t44wt+v%=2jz&x#i$q"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("env", "prod") != "prod"  # !
