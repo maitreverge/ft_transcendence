51dbbdf8e0f86480e08d61a7c89a1a7056b0655a
Dan SYLVAIN <dansylvain84@gmail.com>
2025-02-03 13:14:37 +0100

    updated myscript.sh

--- ultimate_project/my_script.sh
+++ ultimate_project/my_script.sh
@@ -5,7 +5,7 @@
 # pip install django	# installe django (no? seriously?)
 # pip freeze > requirements.txt 	# documente les dependances
 
-pip install virtualenv	# permet d'installer l'environnement virtuel a 42 sans VM
+# pip install virtualenv	# permet d'installer l'environnement virtuel a 42 sans VM
 python3 -m venv env
 source env/bin/activate 
 pip install django
