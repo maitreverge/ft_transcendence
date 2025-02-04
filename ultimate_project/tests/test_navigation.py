60fd9f2341800358715bafca6d2e91c8de1f7785
Dan SYLVAIN <dansylvain84@gmail.com>
2025-02-04 08:59:12 +0100

    updated playwright script for CI/CD

--- ultimate_project/tests/test_navigation.py
+++ ultimate_project/tests/test_navigation.py
@@ -5,7 +5,7 @@ from playwright.sync_api import Playwright, sync_playwright  # , expect
 
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
 
