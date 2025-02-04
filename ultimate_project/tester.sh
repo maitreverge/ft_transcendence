0c9e9835c5aa3ebbd54ce27b0ffa77e023602a6f
Dan SYLVAIN <dansylvain84@gmail.com>
2025-02-04 09:17:46 +0100

    tester updated

--- ultimate_project/tester.sh
+++ ultimate_project/tester.sh
@@ -25,6 +25,7 @@ pip install black flake8 pytest playwright > /dev/null 2>&1
 playwright install > /dev/null 2>&1
 check_result "Dependency installation"
 
+
 # Exécution de Black
 echo "Running Black..."
 black . --check > /dev/null 2>&1
@@ -34,6 +35,35 @@ check_result "Black formatting check"
 echo "Running Flake8..."
 flake8 . > /dev/null 2>&1
 check_result "Flake8 linting"
+
+# Démarrage du serveur
+echo "Starting the server..."
+make clean > /dev/null 2>&1
+make re > /dev/null 2>&1 &  # Lance le serveur en arrière-plan
+
+# Attente que les containers Docker soient en cours d'exécution
+echo "Waiting for Docker containers to be ready..."
+until docker ps --filter "name=ctn_nginx" --filter "status=running" | grep -q "ctn_nginx"; do
+    sleep 1
+done
+echo "Docker containers are up and running!"
+
+# Exécution des tests Playwright
+echo "Running Playwright tests..."
+python3 tests/test_navigation.py > /dev/null 2>&1
+check_result "Playwright tests"
+
+# Démarrage du serveur
+echo "Starting the server..."
+make clean > /dev/null 2>&1
+make prod > /dev/null 2>&1 &  # Lance le serveur en arrière-plan
+
+# Attente que les containers Docker soient en cours d'exécution
+echo "Waiting for Docker containers to be ready..."
+until docker ps --filter "name=ctn_nginx" --filter "status=running" | grep -q "ctn_nginx"; do
+    sleep 1
+done
+echo "Docker containers are up and running!"
 
 # Exécution des tests Playwright
 echo "Running Playwright tests..."
