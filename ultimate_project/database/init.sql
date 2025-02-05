99f5d36e4550592f91d20dedbe7536067e067100
“maitreverge_42” <verge.f@gmail.com>
2025-02-05 16:08:11 +0100

    users schema v1

--- ultimate_project/database/init.sql
+++ ultimate_project/database/init.sql
@@ -1,9 +1,18 @@
+-- BOOL is SQL can be either true, false or NULL
+
 -- USERS SERVICE
 CREATE SCHEMA [IF NOT EXISTS] users;
 CREATE TABLE [IF NOT EXISTS] users.user (
 	"id" INTEGER NOT NULL,
+	"username" TEXT NOT NULL UNIQUE,
+	"email" TEXT UNIQUE, -- See if 42 API fetches student's email adresses make this NOT NULL
+	"password" NOT NULL, -- Passwords need to be salted + hashed + form checks (x characters longs / alphanumeric, ect...)
+	"avatar" BLOB DEFAULT, -- Need a default avatar OR fetch student PP from 42 API
+	"is_online" BOOL NOT NULL,
+	"pref_language" TEXT NOT NULL DEFAULT "en", -- This is to be determined with support-language option
+	"2FA_enabled" BOOL DEFAULT false,
+	"tournament_alias" TEXT,
 	PRIMARY KEY ("id")
-
 )
 
 -- MATCH-MACKING
