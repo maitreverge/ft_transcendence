98de0664a59276e87b97910bb822e380cb9567a1
sierra182 <sierra-echo@msn.com>
2025-02-06 17:52:02 +0100

    rerescript

--- ultimate_project/match/match_app/static/match/script.js
+++ ultimate_project/match/match_app/static/match/script.js
@@ -1,7 +1,4 @@
 
-// const socket = new WebSocket("ws://localhost:8000/ws/somepath/");
-// const socket = new WebSocket( window.rasp + "/ws/somepath/");
-
 if (window.rasp == "true")
 	socket = new WebSocket(`wss://${window.pidom}/ws/somepath/`);
 else
@@ -28,4 +25,3 @@ document.addEventListener("keydown", function(event) {
         }
     }
 });
-
