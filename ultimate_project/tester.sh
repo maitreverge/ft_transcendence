21e735c5ce23a4c37ed62396736bf15f069ef7a3
Dan SYLVAIN <dansylvain84@gmail.com>
2025-02-06 17:11:02 +0100

    test refactored

--- ultimate_project/tester.sh
+++ ultimate_project/tester.sh
@@ -5,9 +5,9 @@ source tests/utils.sh
 python3 --version > /dev/null 2>&1 || { echo -e "${RED}Python3 is not installed!${RESET}"; exit 1; }
 
 ./tests/test_linters.sh
-./tests/start_server.sh
+./tests/start_server.sh re
 ./tests/test_playwright.sh
-./tests/start_server.sh
+./tests/start_server.sh prod
 ./tests/test_playwright.sh
 
 echo -e "${GREEN}All tests passed successfully! 🎉${RESET}"
--- ultimate_project/tests/start_server.sh
+++ ultimate_project/tests/start_server.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# Démarrage du serveur
+if [ -z "$1" ]; then
+    echo "Usage: $0 {re|prod}"
+    exit 1
+fi
+
+MAKE_RULE=$1
+
 echo "Starting the server..."
 make clean > /dev/null 2>&1
-make re > /dev/null 2>&1 &  # Lance le serveur en arrière-plan
+make $MAKE_RULE > /dev/null 2>&1 &  # Lance le serveur en arrière-plan
 
-# Attente que les containers Docker soient en cours d'exécution
 echo "Waiting for Docker containers to be ready..."
 until docker ps --filter "name=ctn_nginx" --filter "status=running" | grep -q "ctn_nginx"; do
     sleep 1
