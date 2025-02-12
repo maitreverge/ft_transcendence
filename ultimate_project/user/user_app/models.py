ef9e83276ff4f1c5deb709574305a684ca799bd4
maitreverge <verge.f@gmail.com>
2025-02-12 16:51:07 +0100

    Creating base model for `Users`

--- ultimate_project/user/user_app/models.py
+++ ultimate_project/user/user_app/models.py
@@ -1,3 +1,19 @@
-# from django.db import models
+from django.db import models
+from django.core.validators import MinLengthValidator
 
-# # Create your models here.
+
+# USER TABLE
+class User(models.Model):
+    username = models.CharField(
+        max_length=42,
+        unique=True,
+        validators=[
+            MinLengthValidator(5, "the field must contain at least 5 characters")
+        ],
+    )
+    password = models. # TODO
+    age = models.PositiveIntegerField(min=18, max=120)
+
+
+    def __str__():
+        return f"User : {self.username}\n"
