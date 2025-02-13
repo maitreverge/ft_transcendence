493f3f9115c8dfd640f455fe3046f4ab613a271e
Dan Sylvain <dansylvain84@gmail.com>
2025-02-13 11:36:19 +0100

    created api_gateway base container

---
+++ ultimate_project/api_gateway/api_gateway_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def api_gateway(request):
+    return HttpResponse(
+        """
+        <div class="overlay">
+            <div class="overlay-content">
+                <h2>api_gateway Oulala</h2>
+            </div>
+        </div>
+    """
+    )
