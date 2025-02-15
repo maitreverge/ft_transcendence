f2ffc0ec6f6488fc95c4f12e3f98d4de7237daae
sierra182 <sierra-echo@msn.com>
2025-02-15 10:28:54 +0100

    wip index to match

--- ultimate_project/tournament/tournament_app/static/tournament/script.js
+++ ultimate_project/tournament/tournament_app/static/tournament/script.js
@@ -1,6 +1,9 @@
 
-document.getElementById("new-match").addEventListener("click", function(event) {
-	event.preventDefault();
-	console.log("ta bien clique man");
-	fetch('http://localhost:8000/match/new-match/').then(response => response.json()).then(data => console.log(data)).catch(error => console.error(error))
-});
+// document.getElementById("new-match").addEventListener("click", function(event) {
+// 	event.preventDefault();
+// 	console.log("ta bien clique man");
+// 	fetch('http://localhost:8000/match/new-match/')
+// 		.then(response => response.json())
+// 		.then(data => console.log(data))
+// 		.catch(error => console.error(error))
+// });
--- ultimate_project/tournament/tournament_app/views.py
+++ ultimate_project/tournament/tournament_app/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import requests
 # Create your views here.
-
+from django.http import JsonResponse
 
 def test(request):
     return render(request, "test.html", {"touille": "champion du bidule"})
 
 
 def start_tournament(request):
-    return render(request, "test.html", {"touille": "champion du bidulee"})
-    return HttpResponse(
+	print("START TOURNAMENT", flush=True)
+	data = requests.get("http://ctn-match:8002/match/new-match/").json() #! opti url and gateway!
+	print(data, flush=True)
+	# return JsonResponse(data, status= 201)
+	return render(request, "test.html", {"matchData": data})
+	return HttpResponse(
         """
         <div class="overlay">
             <div class="overlay-content">
