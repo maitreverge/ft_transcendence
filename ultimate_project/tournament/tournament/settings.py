43329aa38f13c0aabae00346f8b41cbbe3f5a1c1
Dan SYLVAIN <dansylvain84@gmail.com>
2025-02-18 06:55:41 +0100

    transfer success!

--- ultimate_project/tournament/tournament/settings.py
+++ ultimate_project/tournament/tournament/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("env", "prod") != "prod"
 
-ALLOWED_HOSTS = ["*", f"https://{PI_DOMAIN}"]
+ALLOWED_HOSTS = ["*", "ctn_tournament:8001", "ctn_tournament", "ctn_api_gateway", "api_gateway"]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8000",
