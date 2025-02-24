ee01a3d0bb46ed7b4fc22f39e13246ac3f4d687f
maitreverge <verge.f@gmail.com>
2025-02-24 08:56:57 +0100

    Delete `admin` app

--- ultimate_project/admin/admin_app/__init__.py
+++
@@ -1,3 +0,0 @@
-# admin_app/__init__.py
-
-default_app_config = 'admin_app.apps.AdminAppConfig'
--- ultimate_project/admin/admin_app/apps.py
+++
@@ -1,7 +0,0 @@
-# admin_app/apps.py
-
-from django.apps import AppConfig
-
-class AdminAppConfig(AppConfig):
-    name = 'admin_app'
-    label = 'admin_app'
