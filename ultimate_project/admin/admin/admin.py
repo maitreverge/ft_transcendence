96035e13b97248e01ee371bacd91471772fefc2c
maitreverge <verge.f@gmail.com>
2025-02-24 15:32:37 +0100

    `pdadmin` connecting to database

--- ultimate_project/admin/admin/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import AuthDummyModel, UserManagementDummyModel, MatchDummyModel, TournamentDummyModel
-
-# Register your models here.
-admin.site.register(AuthDummyModel)
-admin.site.register(UserManagementDummyModel)
-admin.site.register(MatchDummyModel)
-admin.site.register(TournamentDummyModel)
