d92cbc13a57140b30cd39e541aa9b39714fe7096
maitreverge <verge.f@gmail.com>
2025-02-25 10:12:21 +0100

    wip Foreigns keys accross containers

--- ultimate_project/tournament/tournament_app/models.py
+++ ultimate_project/tournament/tournament_app/models.py
@@ -1,40 +1,54 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 
+
 # Create your models here.
 class Player(AbstractBaseUser):
     id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, unique=True)
-    username = models.CharField(max_length=100, unique=True)
-    password = models.CharField(max_length=100)
+    password = models.CharField(max_length=100, blank=True)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    username = models.CharField(max_length=100, unique=True, blank=True)
+    is_active = models.BooleanField(default=True)
 
+    USERNAME_FIELD = "email"
 
     class Meta:
         managed = False
-        db_table = 'user_schema.player'
-    
+        db_table = "user_schema.player"
+
     def __str__(self):
-        return self.first_name + ' ' + self.last_name
+        return self.first_name + " " + self.last_name
 
 
 class Match(models.Model):
     id = models.AutoField(primary_key=True)
+    player1 = models.ForeignKey(
+        Player, on_delete=models.CASCADE, related_name="player1"
+    )
+    player2 = models.ForeignKey(
+        Player, on_delete=models.CASCADE, related_name="player2"
+    )
+    winner = models.ForeignKey(Player, on_delete=models.CASCADE, related_name="winner")
+    tournament = models.ForeignKey(
+        "Tournament", on_delete=models.SET_NULL, null=True, blank=True
+    )
 
     class Meta:
         managed = False
-        db_table = 'match_schema.match'
+        db_table = "match_schema.match"
 
     def __str__(self):
         pass
+
 
 class Tournament(models.Model):
     id = models.AutoField(primary_key=True)
 
     class Meta:
         # managed = False
-        db_table = 'tournament_schema.tournament'
-    
+        db_table = "tournament_schema.tournament"
+
     def __str__(self):
-        pass
+        return f"Tournament {self.id}"
