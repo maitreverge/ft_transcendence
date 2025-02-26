efadcdfeee97e716be75fe8414115f32bfbc912e
Dan SYLVAIN <dansylvain84@gmail.com>
2025-02-26 10:52:18 +0100

    added hx-push-url=true to all hx-get

--- ultimate_project/static_files/static_files_app/urls.py
+++ ultimate_project/static_files/static_files_app/urls.py
@@ -5,10 +5,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login-form/", views.login_form, name="login-form"),
     path("login/", views.login, name="login"),
-    path("tournament/", views.tournament, name="tournament"),
-    path("simple-match/", views.simple_match, name="simple-match"),
     path("home/", views.home),
     path("user/profil/", views.profil),
     path("user/stats/", views.stats),
-
 ]
--- ultimate_project/static_files/static_files_app/views.py
+++ ultimate_project/static_files/static_files_app/views.py
@@ -8,55 +8,14 @@ def index(request):
     username = request.session.get("username")
     return render(request, "index.html", {"username": username})
 
-
 def login_form(request):
     csrf_token = csrf.get_token(request)
     return render(request, "login.html")
-    template = Template(
-        """
-        <form hx-post="/login/" hx-target="body">
-            {% csrf_token %}
-            <input type="text" name="username" placeholder="Entrez votre nom">
-            <button type="submit">Connexion</button>
-        </form>
-    """
-    )
-    context = Context({"csrf_token": csrf_token})
-    return HttpResponse(template.render(context))
-
 
 def login(request):
     username = request.POST.get("username")
     request.session["username"] = username
     return redirect("/")
-
-
-def tournament(request):
-    return HttpResponse(
-        """
-        <div class="overlay">
-            <div class="overlay-content">
-                <h2>Tournament creation</h2>
-                <!-- Tournament content -->
-                <button hx-get="/" hx-target="body">Close</button>
-            </div>
-        </div>
-    """
-    )
-
-
-def simple_match(request):
-    return HttpResponse(
-        """
-        <div class="overlay">
-            <div class="overlay-content">
-                <h2>Simple Match</h2>
-                <!-- match content -->
-                <button hx-get="/" hx-target="body">Close</button>
-            </div>
-        </div>
-    """
-    )
 
 def home(request):
     return render(request, "home.html")
