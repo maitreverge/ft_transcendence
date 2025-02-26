c1a346584832abd4c5adb5b58052d4d8f35408fc
maitreverge <verge.f@gmail.com>
2025-02-26 16:38:01 +0100

    Kickstarting basic auth

--- ultimate_project/user/auth_app/views.py
+++ ultimate_project/user/auth_app/views.py
@@ -7,14 +7,22 @@ from django.urls import reverse
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
+    return render(request, "auth_app/login.html")
 
 def login_view(request):
     if request.method == "POST":
         pass
-    return render(request, "auth_app/login.html")
+        # print(list(request.POST.items()), flush=True)
+    return render(request, "auth_app/login.html", {
+        "title": "LOGIN PAGE"
+    })
 
 def signin_view(request):
-    pass
+    if request.method == "POST":
+        pass
+    return render(request, "auth_app/signin.html", {
+        "title": "SIGNIN PAGE"
+    })
 
 def logout_view(request):
     pass
