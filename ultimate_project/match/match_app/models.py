3281d19e9d3607e184c6f88224531d6b762cacee
maitreverge <verge.f@gmail.com>
2025-02-28 12:00:21 +0100

    Create `2fa` key

--- ultimate_project/match/match_app/models.py
+++ ultimate_project/match/match_app/models.py
@@ -23,6 +23,8 @@ class Player(CrossSchemaModel):
     # 2FA fields    
     two_fa_enabled = models.BooleanField(default=False)  # 2FA toggle
     two_fa_verified = models.BooleanField(default=False)  # 2FA verification status
+    two_fa_secret = models.CharField(max_length=32, blank=True, null=True)  # ✅ Store 2FA secret
+
 
     # Tells Django to use "email" as the primary field for authentication
     USERNAME_FIELD = "username"
--- ultimate_project/user/auth_app/two_fa.py
+++ ultimate_project/user/auth_app/two_fa.py
@@ -1,6 +1,25 @@
+import pyotp
+import qrcode
+import io
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from user_management_app.models import Player
 from django.http import HttpResponse
+
+
 def verify_2fa(request):
     pass
 
+
+@login_required
 def enable_2fa(request):
-    return HttpResponse("This is the 2FA setup page")
+    
+    # Generate a new secret key
+    secret = pyotp.random_base32()
+
+    
+
+    return render(request, "auth_app/enable_2fa.html", {
+        "title": "This is 2FA setup",
+        "secret": secret,
+    })
