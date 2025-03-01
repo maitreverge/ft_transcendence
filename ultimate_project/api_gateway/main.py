2862eb02c23fcce6b8767e38ba6d2c30d844c774
Dan SYLVAIN <dansylvain84@gmail.com>
2025-03-01 13:37:55 +0100

    websocket connection established

--- ultimate_project/api_gateway/main.py
+++ ultimate_project/api_gateway/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, WebSocket
 import httpx
 from fastapi.responses import HTMLResponse
 
@@ -8,7 +8,7 @@ services = {
     "static_files": "http://static_files:8003",
     "tournament": "http://tournament:8001",
     "match": "http://match:8002",
-    "static_tournament": "http://tournament:8001/static",
+    "ws_tournament": "http://tournament:8001/ws/tournament",
 }
 
 async def proxy_request(service_name: str, path: str, request: Request):
@@ -29,25 +29,45 @@ async def proxy_request(service_name: str, path: str, request: Request):
             return HTMLResponse(content=response.text)
         return response.text
 
+
+# 	location /ws/tournament/ {
+# 		proxy_pass http://ctn_tournament:8001/ws/tournament/; 
+# 		proxy_http_version 1.1;                 # ✅ WebSocket nécessite HTTP 1.1
+#         proxy_set_header Upgrade $http_upgrade; # ✅ Indique que c'est un WebSocket
+#         proxy_set_header Connection "Upgrade";  # ✅ Autorise la montée en protocole
+#         proxy_set_header Host $host;
+#         proxy_set_header X-Real-IP $remote_addr;
+#         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+#         proxy_set_header X-Forwarded-Proto $scheme;
+# 	}
+
+@app.websocket("/ws/tournament/")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
+
+@app.api_route("/ws/tournament/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
+async def tournament_proxy(path: str, request: Request):
+    return await proxy_request("ws_tournament", path, request)
+
 @app.api_route("/tournament/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
 async def tournament_proxy(path: str, request: Request):
     return await proxy_request("tournament", path, request)
-
-@app.api_route("/static/tournament/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
-async def tournament_proxy(path: str, request: Request):
-    return await proxy_request("static_tournament", path, request)
-
-@app.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
-async def static_files_proxy(path: str, request: Request):
-    return await proxy_request("static_files", path, request)
 
 @app.api_route("/match/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
 async def match_proxy(path: str, request: Request):
     return await proxy_request("match", path, request)
 
-@app.api_route("/user/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
-async def user_proxy(path: str, request: Request):
-    return await proxy_request("user", path, request)
+    # @app.api_route("/user/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
+    # async def user_proxy(path: str, request: Request):
+    #     return await proxy_request("user", path, request)
+
+@app.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
+async def static_files_proxy(path: str, request: Request):
+    return await proxy_request("static_files", path, request)
+
 
 if __name__ == "__main__":
     import uvicorn
