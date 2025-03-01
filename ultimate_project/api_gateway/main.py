126782074b912621f876e3513a1ccb4593992699
Dan SYLVAIN <dansylvain84@gmail.com>
2025-03-01 12:14:07 +0100

    cleaned some files

--- ultimate_project/api_gateway/main.py
+++ ultimate_project/api_gateway/main.py
@@ -8,17 +8,15 @@ services = {
     "static_files": "http://static_files:8003",
     "tournament": "http://tournament:8001",
     "match": "http://match:8002",
-    "static_tournament": "http://tournament:8001/static/tournament",
+    "static_tournament": "http://tournament:8001/static",
 }
 
 async def proxy_request(service_name: str, path: str, request: Request):
     async with httpx.AsyncClient(follow_redirects=True) as client:
         base_url = services[service_name].rstrip("/")
         path = path.lstrip("/")
-
         url = f"{base_url}/{path}"
 
-        # print("\n**************** \n**************** PROXY API GATEWAY \n****************", url, "\n****************")
         headers = dict(request.headers)
         headers.pop("host", None)
         headers["Host"] = "localhost"
@@ -31,13 +29,6 @@ async def proxy_request(service_name: str, path: str, request: Request):
             return HTMLResponse(content=response.text)
         return response.text
 
-
-# @app.api_route("/tournament/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
-# @app.api_route("/tournament", methods=["GET", "POST", "PUT", "DELETE"])
-# async def tournament_proxy(request: Request, path: str = ""):
-#     full_path = f"/tournament/{path}" if path else "/tournament"
-#     return await proxy_request("tournament", full_path, request)
-
 @app.api_route("/tournament/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
 async def tournament_proxy(path: str, request: Request):
     return await proxy_request("tournament", path, request)
@@ -48,7 +39,6 @@ async def tournament_proxy(path: str, request: Request):
 
 @app.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
 async def static_files_proxy(path: str, request: Request):
-    print(f"🔥 Received static file request: {path}")  # <--- LOG
     return await proxy_request("static_files", path, request)
 
 @app.api_route("/match/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
