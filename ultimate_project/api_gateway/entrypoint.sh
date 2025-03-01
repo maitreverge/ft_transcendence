b804fd9ba8b29e00e3e781f83ea3648771cd5f79
Dan SYLVAIN <dansylvain84@gmail.com>
2025-03-01 12:44:33 +0100

    cleaned up api gateway entrypoint script

--- ultimate_project/api_gateway/entrypoint.sh
+++ ultimate_project/api_gateway/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# if [ "${env}" = "prod" ]; then \
-	mkdir -p /app/staticfiles && chmod -R 777 /app/staticfiles; \
-	# python manage.py collectstatic --noinput; \
-	uvicorn ${name}.asgi:application --host 0.0.0.0 --port ${port}; \
-# else \
-# 	python ./manage.py runserver 0.0.0.0:${port}; \
-# fi
+uvicorn ${name}.asgi:application --host 0.0.0.0 --port ${port}; \
 
 exec "$@"
