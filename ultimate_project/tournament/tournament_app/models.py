3993810409d8be7b3cd956e3d82d9ccc4151430f
maitreverge <verge.f@gmail.com>
2025-03-05 16:49:35 +0100

    Lint models

--- ultimate_project/tournament/tournament_app/models.py
+++ ultimate_project/tournament/tournament_app/models.py
@@ -18,22 +18,21 @@ class Player(CrossSchemaModel):
     first_name = models.CharField(max_length=100, blank=True)
     last_name = models.CharField(max_length=100, blank=True)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)  # needed for admin access
+    is_staff = models.BooleanField(default=False)
 
     # 2FA fields
-    two_fa_enabled = models.BooleanField(default=False)  # 2FA toggle
-    two_fa_verified = models.BooleanField(default=False)  # 2FA verification status
+    two_fa_enabled = models.BooleanField(default=False)
+    two_fa_verified = models.BooleanField(default=False)
     _two_fa_secret = models.CharField(
         max_length=32, blank=True, null=True
     )  # ✅ Store 2FA secret
 
-    # Tells Django to use "email" as the primary field for authentication
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = ["email"]
 
     class Meta:
-        managed = False  # This service is NOT responsible for creating and managing this model
-        db_table = "player"  # Explicitly set the schema and table name
+        managed = False 
+        db_table = "player"
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -43,7 +42,7 @@ class Tournament(CrossSchemaModel):
     id = models.AutoField(primary_key=True)
 
     class Meta:
-        managed = False  # This service is NOT responsible for creating and managing this model
+        managed = False
         db_table = "tournament"
 
     def __str__(self):
@@ -54,9 +53,9 @@ class Match(CrossSchemaModel):
     id = models.AutoField(primary_key=True)
 
     player1 = models.ForeignKey(
-        to=Player,  # Explicit reference to Player model in the `user` app
-        on_delete=models.CASCADE,  # If a player is deleted, the match is also deleted
-        related_name="player1",  # Allows querying Match objects where the player was player1
+        to=Player,
+        on_delete=models.CASCADE,
+        related_name="player1",
     )
     player2 = models.ForeignKey(
         to=Player,
@@ -66,12 +65,12 @@ class Match(CrossSchemaModel):
     winner = models.ForeignKey(
         to=Player,
         on_delete=models.CASCADE,
-        related_name="winner",  # Allows querying Match objects where this player was the winner
+        related_name="winner",
     )
 
     tournament = models.ForeignKey(
-        to=Tournament,  # Explicit reference to Tournament model from tournament service
-        on_delete=models.SET_NULL,  # If the tournament is deleted, set this field to NULL
+        to=Tournament,
+        on_delete=models.SET_NULL,
         null=True,
         blank=True,
     )
