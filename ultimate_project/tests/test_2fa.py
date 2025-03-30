b416d7e1a5fe641d7ce023585e315b48b3522979
maitreverge <verge.f@gmail.com>
2025-03-30 09:55:34 +0200

    register correct

--- ultimate_project/tests/test_2fa.py
+++ ultimate_project/tests/test_2fa.py
@@ -6,28 +6,36 @@ import pyotp
 test_2fa_secret = "S3EF2KESUQR45MRTL7MXDSJVI6JQDG4R"
 
 
-register_2fa_test_username = "register-2fa-test"
-register_2fa_test_email = "register-2fa-test@test.com"
-register_2fa_test_password = "password"
+BASE_URL = "https://localhost:8443"
+
+REGISTER_USERNAME = "register-2fa-test"
+REGISTER_EMAIL = "register-2fa-test@test.com"
+PASSWORD = "password"
 
 # ! =============== TESTING 2FA ===============
 
-base_url = "https://localhost:8443"
 
 def run(playwright: Playwright) -> None:
 
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(
-        ignore_https_errors=True
-    )
+    context = browser.new_context(ignore_https_errors=True)
     page = context.new_page()
 
+    def logout():
+        youpiBanane = page.locator("#youpiBanane")
+        logoutButton = page.locator("#logoutButton")
+        modalLogoutButton = page.locator("#modalLogoutButton")
+        assert "show" not in (youpiBanane.get_attribute("class") or "")
+        youpiBanane.click()
+        logoutButton.click()
+        modalLogoutButton.click()
+        expect(page).to_have_url(f"{BASE_URL}/login/")
 
     def get_ordinal_suffix(num):
         if num == 1:
             return "st"
         elif num == 2:
-            return "nd" 
+            return "nd"
         elif num == 3:
             return "rd"
         else:
@@ -37,22 +45,22 @@ def run(playwright: Playwright) -> None:
         # Create a TOTP object
         totp = pyotp.TOTP(test_2fa_secret)
 
-        page.goto(f"{base_url}/login/")
+        page.goto(f"{BASE_URL}/login/")
 
         # ! ============= LOGIN PAGE =============
         # Fill in the username and password
-        expect(page).to_have_url(f"{base_url}/login/")
+        expect(page).to_have_url(f"{BASE_URL}/login/")
         page.locator("#username").fill("test_2fa")
         page.locator("#password").fill("password")
         page.locator("#loginButton").click()
 
         # ! ============= TWO-FA PAGE =============
-        expect(page).to_have_url(f"{base_url}/two-factor-auth/")
+        expect(page).to_have_url(f"{BASE_URL}/two-factor-auth/")
 
         # Fill with a wrong code
         page.locator("#otp_input").fill("000000")
         page.locator("#otp_verify").click()
-        expect(page).to_have_url(f"{base_url}/two-factor-auth/")
+        expect(page).to_have_url(f"{BASE_URL}/two-factor-auth/")
         error_message = page.locator("#login_error")
         expect(error_message).to_have_text("Invalid 2FA code")
 
@@ -60,7 +68,7 @@ def run(playwright: Playwright) -> None:
         page.locator("#cancel_button").click()
 
         # ! ============= LOGIN PAGE =============
-        expect(page).to_have_url(f"{base_url}/login/")
+        expect(page).to_have_url(f"{BASE_URL}/login/")
 
         # Fill in the username and password
         page.locator("#username").fill("test_2fa")
@@ -68,7 +76,7 @@ def run(playwright: Playwright) -> None:
         page.locator("#loginButton").click()
 
         # ! ============= TWO-FA PAGE =============
-        expect(page).to_have_url(f"{base_url}/two-factor-auth/")
+        expect(page).to_have_url(f"{BASE_URL}/two-factor-auth/")
 
         # Get the current code
 
@@ -78,43 +86,42 @@ def run(playwright: Playwright) -> None:
             try:
                 page.locator("#otp_input").fill(current_code)
                 page.locator("#otp_verify").click()
-                expect(page).to_have_url(f"{base_url}/home/")
-                print(f"✅ 2FA connexion succed on {_ + 1}{get_ordinal_suffix(_ + 1)} try ✅", flush=True)
+                expect(page).to_have_url(f"{BASE_URL}/home/")
+                print(
+                    f"✅ 2FA connexion succed on {_ + 1}{get_ordinal_suffix(_ + 1)} try ✅",
+                    flush=True,
+                )
                 break
             except Exception as e:
                 print(f"💀 2FA connexion failed {_ + 1} times, retrying 💀", flush=True)
+        
+        logout()
 
+    def test_register_2fa():
 
-    def test_register_2fa(playwright: Playwright):
-        # Starting a new window
-        # browser = playwright.chromium.launch(headless=False)
-        # context = browser.new_context(
-        #     ignore_https_errors=True
-        # )
-        # page = context.new_page()
-        page.goto(f"{base_url}/register/")
+        page.goto(f"{BASE_URL}/register/")
 
         # ! ============= REGISTER PAGE =============
-        expect(page).to_have_url(f"{base_url}/register/")
+        expect(page).to_have_url(f"{BASE_URL}/register/")
 
         # Fill in the username and password
         page.locator("#first_name").fill("test")
         page.locator("#last_name").fill("test")
-        page.locator("#username").fill(register_2fa_test_username)
-        page.locator("#email").fill(register_2fa_test_email)
-        page.locator("#password").fill(register_2fa_test_password)
-        page.locator("#repeat_password").fill(register_2fa_test_password)
+        page.locator("#username").fill(REGISTER_USERNAME)
+        page.locator("#email").fill(REGISTER_EMAIL)
+        page.locator("#password").fill(PASSWORD)
+        page.locator("#repeat_password").fill(PASSWORD)
 
         page.locator("#register-button").click()
 
         # ! ============= HOME PAGE =============
-        expect(page).to_have_url(f"{base_url}/home/")
+        expect(page).to_have_url(f"{BASE_URL}/home/")
 
         # Go to profile page
         page.locator("#nav-profile").click()
 
         # ! ============= PROFILE PAGE =============
-        expect(page).to_have_url(f"{base_url}/user/profile/")
+        expect(page).to_have_url(f"{BASE_URL}/user/profile/")
 
         # Check is 2FA is not enabled
         expect(page.locator("#disable_2fa")).to_be_hidden()
@@ -125,32 +132,40 @@ def run(playwright: Playwright) -> None:
 
         # ! ============= TWO-FA PAGE =============
 
-        extracted_secret = page.locator("#secret_key").text_content()
-        print(extracted_secret, flush=True)
-        totp = pyotp.TOTP(extracted_secret)
+        EXTRACTED_SECRET = page.locator("#secret_key").text_content()
+        print(f"🐛🐛🐛 CURRENT 2FA : {EXTRACTED_SECRET}", flush=True)
+        totp = pyotp.TOTP(EXTRACTED_SECRET)
 
-        # Wait for the input field to be visible and fill it
-        otp_input = page.locator("#otp_input")
-        # otp_input.wait_for(state="visible", timeout=5000)
-        otp_input.fill(totp.now())
+        # TRY TO SETUP 2FA
+        for _ in range(3):
+            try:
+                # Wait for the input field to be visible and fill it
+                otp_input = page.locator("#otp_input")
+                # otp_input.wait_for(state="visible", timeout=5000)
+                otp_input.fill(totp.now())
 
-        # Wait for the verify button to be visible and clickable
-        verify_button = page.locator("#otp_verify")
-        # verify_button.wait_for(state="visible", timeout=5000)
-        # verify_button.wait_for(state="enabled", timeout=5000)
-        verify_button.click()
+                # Wait for the verify button to be visible and clickable
+                verify_button = page.locator("#otp_verify")
+                # verify_button.wait_for(state="visible", timeout=5000)
+                # verify_button.wait_for(state="enabled", timeout=5000)
+                verify_button.click()
 
-        # Wait for success message to be visible
-        success_message = page.locator("#twofa_success_message")
-        success_message.wait_for(state="visible", timeout=5000)
-        expect(success_message).to_have_text("2FA Action Complete")
+                # Wait for success message to be visible
+                success_message = page.locator("#twofa_success_message")
+                success_message.wait_for(state="visible", timeout=5000)
+                expect(success_message).to_have_text("2FA Action Complete")
 
-        # Wait for log message to be visible
-        log_message = page.locator("#log_message")
-        log_message.wait_for(state="visible", timeout=5000)
-        expect(log_message).to_have_text(
-            "Your account is now protected with two-factor authentication."
-        )
+                # Wait for log message to be visible
+                log_message = page.locator("#log_message")
+                log_message.wait_for(state="visible", timeout=5000)
+                expect(log_message).to_have_text(
+                    "Your account is now protected with two-factor authentication."
+                )
+                break
+            except Exception as e:
+                print(f"💀 2FA connexion failed {_ + 1} times, retrying 💀", flush=True)
+        
+        logout()
 
         # # Click on disable 2FA
         # page.locator("#disable_2fa").click()
@@ -160,10 +175,10 @@ def run(playwright: Playwright) -> None:
 
         # expect(page.locator("#success_message")).to_have_text("Two-factor authentication has been disabled for your account.")
 
-        # expect(page).to_have_url(f"{base_url}/user/profile/")
-        
+        # expect(page).to_have_url(f"{BASE_URL}/user/profile/")
+
     # ! =============== KICKSTART TESTER HERE ===============
-    
+
     test_login_2fa()
 
     test_register_2fa()
