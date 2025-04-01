fbfc2c8b535b37b1d14ca4b38f3c6e1cc453611f
maitreverge <verge.f@gmail.com>
2025-04-01 17:14:30 +0200

    Tested `username` in tournament and simple match

--- ultimate_project/tests/test_username.py
+++ ultimate_project/tests/test_username.py
@@ -1,38 +1,32 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
 from collections import Counter
 import time
-import os
 
 # USERS
-# LOGIN_REG = "same_auth"
-# LOGIN_2FA = "same_auth_2fa"
-# PASSWORD = "password"
-# SECRET_2FA = "9JUF2KESUQR45MRTL7MXDSJVI6JQDG42"
+LOGIN_REG = "user2"
+PASSWORD = "password"
 
-SIMULTANEOUS_USERS = 2
-
+SENTENCE = "Je suis "
 BASE_URL = "https://localhost:8443"
 
-
 def run(playwright: Playwright) -> None:
-    # browser = playwright.chromium.launch(headless=False)
-    # context = browser.new_context(
-    #     ignore_https_errors=True
-    # )
-    # page = context.new_page()
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(
+        ignore_https_errors=True
+    )
+    page = context.new_page()
 
-    def login(page, login):
+    def login():
         expect(page).to_have_url(f"{BASE_URL}/login/")
 
         # Fill in the username and password
-        page.locator("#username").fill(login)
+        page.locator("#username").fill(LOGIN_REG)
         page.locator("#password").fill(PASSWORD)
         page.locator("#loginButton").click()
 
-        # ! ============= TWO-FA PAGE =============
         expect(page).to_have_url(f"{BASE_URL}/home/")
 
-    def logout(page):
+    def logout():
         youpiBanane = page.locator("#youpiBanane")
         logoutButton = page.locator("#logoutButton")
         modalLogoutButton = page.locator("#modalLogoutButton")
@@ -42,43 +36,38 @@ def run(playwright: Playwright) -> None:
         modalLogoutButton.click()
         expect(page).to_have_url(f"{BASE_URL}/login/")
 
+    def test_view(locator):
+        page.goto(f"{BASE_URL}/login/")
+
+        login()
+
+        page.locator(f"#{locator}").click()
+
+        # Extract username
+        username = page.locator("#player").text_content().removeprefix("Je suis ")
+
+        # Assertion on the first try
+        assert username == LOGIN_REG
+
+        page.reload()
+        assert username == LOGIN_REG
+        
+        time.sleep(1)
+        
+        page.reload()
+        assert username == LOGIN_REG
+
+        logout()
 
     # ! =============== KICKSTART TESTER HERE ===============
-    browsers = []
-    contexts = []
-    pages = []
-    
 
-    screen_width, screen_height = get_screen_size()
+    test_view("nav-match")
+    test_view("nav-tournoi")
 
-    # Make windows narrow but tall (vertical shape)
-    window_width = int(screen_width * 0.35)  # 35% of screen width
-    window_height = int(screen_height * 0.9)  # 90% of screen height
+    print(f"✅ USERNAME PASSED INTO MATCH / TOURNAMENT ✅")
 
-    # Position one window at far left, one at far right
-    left_position = 0
-    right_position = screen_width - window_width
-
-    # Position windows at left and right edges with different Y positions
-    positions = [(left_position, 20), (right_position, 20)]
-
-    # Better debugging to check what's happening
-    print(f"Screen dimensions: {screen_width}x{screen_height}")
-    print(f"Window size: {window_width}x{window_height}")
-    print(f"Left window position: ({left_position}, 20)")
-    print(f"Right window position: ({right_position}, 20)")
-
-    # Set each window to be the same size
-    window_sizes = [(window_width, window_height), (window_width, window_height)]
-
-    test_reg_users(browsers, contexts, pages, positions, window_sizes)
-
-    test_2fa_users()
-
-    print(f"✅ SAMETIME AUTH PASSED ✅")
-
-    # context.close()
-    # browser.close()
+    context.close()
+    browser.close()
 
 
 with sync_playwright() as playwright:
