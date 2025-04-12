5f52c3e0bd614ca5dacd90c57c7219dd32e9066f
Dan Sylvain <dansylvain84@gmail.com>
2025-04-12 12:15:57 +0200

    htmx request on home page bug fixed

--- ultimate_project/static_files/static_files_app/static/static_files/background_image.js
+++ ultimate_project/static_files/static_files_app/static/static_files/background_image.js
@@ -1,4 +1,4 @@
-window.currentBg = null; // Variable pour stocker l'image de fond actuelle
+// window.currentBg = null; // Variable pour stocker l'image de fond actuelle
 
 // Applique l'image de fond et les styles au chargement ou après un swap HTMX
 if (typeof window.applyBackground === 'undefined') {
@@ -20,11 +20,8 @@ document.querySelectorAll('.side-nav').forEach(item => {
         const bg = item.getAttribute('data-bg');
         // console.log("bg 1: ", bg)
         window.currentBg = bg;
-        const main = document.getElementById('main_content');
-        main.style.backgroundImage = `url(${bg})`;
-        main.style.backgroundSize = 'cover';
-        main.style.backgroundPosition = 'center';
-        main.style.transition = 'background-image 0.3s ease';
+        window.applyBackground(bg);
+
     });
 });
 
@@ -39,18 +36,18 @@ window.addEventListener('DOMContentLoaded', () => {
         const bg = firstItem.getAttribute('data-bg');
         window.currentBg = bg;
         // console.log("bg 2: ", bg)
-        const main = document.getElementById('main_content');
-        main.style.backgroundImage = `url(${bg})`;
-        main.style.backgroundSize = 'cover';
-        main.style.backgroundPosition = 'center';
-        main.style.transition = 'background-image 0.3s ease';
+        window.applyBackground(bg);
     }
 });
 
 document.body.addEventListener('htmx:afterSwap', () => {
-    // console.log("%%%%%%%% HTMX content swapped %%%%%%%%");
-    if (currentBg) {
-        applyBackground(window.currentBg);
-     } // Réapplique l'image de fond stockée
+    console.log("%%%%%%%% HTMX content swapped %%%%%%%%");
+    if (window.currentBg) {
+        // Attendre un court instant avant d'appliquer l'image de fond
+        setTimeout(() => {
+            window.applyBackground(window.currentBg); // Réappliquer l'image de fond
+        }, 100); // Attendre 100ms avant d'appliquer l'image de fond
+    }
+     
     // Si tu as des styles CSS spécifiques qui ne sont pas appliqués, tu peux les réinitialiser ici
 });
