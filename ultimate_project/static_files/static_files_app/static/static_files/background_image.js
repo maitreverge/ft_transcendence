d4c0371c5d29286cf0259d05d1d4d81c2ce67aab
Dan Sylvain <dansylvain84@gmail.com>
2025-04-12 10:42:45 +0200

    css back after htmx request

--- ultimate_project/static_files/static_files_app/static/static_files/background_image.js
+++ ultimate_project/static_files/static_files_app/static/static_files/background_image.js
@@ -1,10 +1,36 @@
-document.querySelectorAll('.nav-item').forEach(item => {
+document.querySelectorAll('.side-nav').forEach(item => {
     item.addEventListener('mouseenter', () => {
         const bg = item.getAttribute('data-bg');
+        console.log("bg 1: ", bg)
+
         const main = document.getElementById('main_content');
         main.style.backgroundImage = `url(${bg})`;
         main.style.backgroundSize = 'cover';
         main.style.backgroundPosition = 'center';
         main.style.transition = 'background-image 0.3s ease';
     });
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    console.log("%%%%%%%% func onload executed %%%%%%%%");
+    const firstItem = document.querySelector('.side-nav');
+    console.log("first item: ", firstItem);
+
+    if (firstItem) {
+        console.log("%%%%%%%% first item found %%%%%%%%");
+
+        const bg = firstItem.getAttribute('data-bg');
+        console.log("bg 2: ", bg)
+        const main = document.getElementById('main_content');
+        main.style.backgroundImage = `url(${bg})`;
+        main.style.backgroundSize = 'cover';
+        main.style.backgroundPosition = 'center';
+        main.style.transition = 'background-image 0.3s ease';
+    }
+});
+
+document.body.addEventListener('htmx:afterSwap', () => {
+    console.log("%%%%%%%% HTMX content swapped %%%%%%%%");
+    applyBackground(); // réapplique l'image de fond
+    // Si tu as des styles CSS spécifiques qui ne sont pas appliqués, tu peux les réinitialiser ici
 });
