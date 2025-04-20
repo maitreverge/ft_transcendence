82c2f06a04ea33fd1c5e21d85c0db3dae1ed4a35
Dan Sylvain <dansylvain84@gmail.com>
2025-04-20 13:27:30 +0200

    check-navbar added

---
+++ ultimate_project/static_files/static_files_app/static/static_files/js/check-navbar.js
@@ -0,0 +1,15 @@
+document.body.addEventListener("htmx:afterSettle", () => {
+    const navbarAbsent = !document.getElementById("navbar");
+    const isHomeUrl = window.location.pathname === "/home/";
+
+    if (navbarAbsent && isHomeUrl) {
+        console.log ("HOLLLY shit! there is no navbar!");
+        htmx.ajax('GET', '/home/', {
+        target: 'body',
+        pushURL: '/home/',
+        headers: {
+            'HX-Login-Success': 'true'
+          }
+        });
+    }
+});
